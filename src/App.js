7e500cccf1648524f98e674a8459df5e171e8fcc
Abhi010bh <git21cs127-t@students.git.edu>
2023-10-21 20:53:41 +0530

    first few

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
+import React from 'react'; // Import React
 import logo from './logo.svg';
 import './App.css';
+import Comp1 from './components/comp1';
 
 function App() {
+  const header = () => (
+    <div>Hello</div>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {header()} {/* Call the 'header' function */}
+      <Comp1 />
     </div>
   );
 }
---
+++ src/components/comp1.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Comp1=()=> {
+  return (
+    <div>Hello from Comp1</div>
+  );
+}
+
+export default Comp1;
